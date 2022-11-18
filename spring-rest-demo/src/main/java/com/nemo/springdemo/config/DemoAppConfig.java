0542cd43dea52cbb4a012b1a60b15369287ea310
nima ghaferi <nima.ghaferi@gmail.com>
2022-11-19 01:06:53 +0330

    config and  hello world service

---
+++ spring-rest-demo/src/main/java/com/nemo/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,11 @@
+package com.nemo.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.nemo.springdemo")
+public class DemoAppConfig {
+}
