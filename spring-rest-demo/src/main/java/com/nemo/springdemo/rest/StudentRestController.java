3ef8b32ca309730ed901472c5084b2ddbee0b38e
nima ghaferi <nima.ghaferi@gmail.com>
2023-02-09 20:35:14 +0330

    json @PathVariable

--- spring-rest-demo/src/main/java/com/nemo/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/nemo/springdemo/rest/StudentRestController.java
@@ -2,9 +2,11 @@ package com.nemo.springdemo.rest;
 
 import com.nemo.springdemo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,19 +14,27 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
         Student s1 = new Student("Nemo", "G");
         Student s2 = new Student("Nil", "G");
         Student s3 = new Student("Minif", "F");
         Student s4 = new Student("Hana", "Gol");
-
-        List<Student> students = new ArrayList<>();
         students.add(s1);
         students.add(s2);
         students.add(s3);
         students.add(s4);
-
+    }
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 }
