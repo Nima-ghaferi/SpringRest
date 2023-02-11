003708b82ba93db4681ec555e9e74b42c570ca6c
nima ghaferi <nima.ghaferi@gmail.com>
2023-02-11 12:27:04 +0330

    spring rest crm delete customer

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -41,4 +41,16 @@ public class CustomerRestController {
         customerService.saveCustomer(customer);
         return customer;
     }
+
+    @DeleteMapping("customers/{customerId}")
+    public String deleteCustomer (@PathVariable int customerId) {
+        Customer customer = customerService.getCustomer(customerId);
+
+        if (customer == null)
+            throw new CustomerNotFoundException("Customer id not found - " + customerId);
+
+        customerService.deleteCustomer(customerId);
+
+        return "Deleted customer id - " + customerId;
+    }
 }
